81c84bdced56bd28e5db951c138c577eeeef09ca
David Stolarsky <david.stolarsky@gmail.com>
2017-11-12 16:39:06 -0500

    getting image dimensions

--- src/chapter_html.js
+++ src/chapter_html.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 const path = require('path');
 
 const getCss = new Promise((resolve, reject) => {
-    fs.readFile(path.join(__dirname, 'styles.css'), {encoding:'utf8'}, (err, data) => {
-        if(err) {
+    fs.readFile(path.join(__dirname, 'styles.css'), { encoding: 'utf8' }, (err, data) => {
+        if (err) {
             reject('Could not read styles.css');
         }
         else {
@@ -13,15 +13,16 @@ const getCss = new Promise((resolve, reject) => {
     });
 });
 
-module.exports = imageFilenames => {
+module.exports = images => {
     return new Promise((resolve, reject) => {
         getCss.then(css => {
             resolve(`
                 <style>${css}</style>
-                ${imageFilenames.map(imageFilename => {
-                    const imageUrl = `static/${imageFilename}`;
+                ${images.map(image => {
+                    const imageUrl = `static/${image.filename}`;
                     return `
                         <img class="photo" src="${imageUrl}"/>
+                        ${image.metadata.width} x ${image.metadata.height}
                     `;
                 }).join('\n')}
             `);
---
+++ src/photochapter.js
@@ -0,0 +1,112 @@
+const child_process = require('child_process');
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const sharp = require('sharp');
+
+const chapterHtml = require('./chapter_html');
+
+// should be more rigorous lol
+const isImage = filename => filename !== '.DS_Store';
+
+const getImagesMetadatas = imagePaths => Promise.all(
+    imagePaths.map(imagePath =>
+        sharp(imagePath)
+            .metadata()
+            .then(function (metadata) {
+                return metadata;
+            })
+    )
+);
+
+module.exports = (sourceDirectory, options) => {
+    options = options || {};
+
+    return new Promise((resolve, reject) => {
+
+        const outputFilePath = path.join(
+            process.cwd(),
+            path.parse(sourceDirectory).name + '.pdf'
+        );
+        console.log(outputFilePath);
+
+        if (!options.dryRun) {
+            const port = 3000;
+            const app = express();
+            app.get('/', (req, res) => {
+
+                fs.readdir(sourceDirectory, (err, possibleImagePaths) => {
+                    if(err) {
+                        reject(`Could not list files in directory ${sourceDirectory}`);
+                    }
+                    else {
+                        const imageFilenames = possibleImagePaths.filter(isImage).map(
+                            imagePath => path.parse(imagePath).base
+                        );
+                        const imagePaths = imageFilenames.map(
+                            imageFilename => path.join(sourceDirectory, imageFilename)
+                        );
+                        
+                        getImagesMetadatas(imagePaths)
+                        .then(imageMetadatas => {
+                            return imageMetadatas.map(
+                                (imageMetadata, i) => ({
+                                    metadata: imageMetadata,
+                                    filename: imageFilenames[i]
+                                })
+                            );
+                        })
+                        .then(chapterHtml)
+                        .then(html => {
+                            res.send(html);
+                        }).catch(err => {
+                            reject(err);
+                        });
+                        
+                    }
+                });
+
+            });
+            app.use('/static', express.static(sourceDirectory));
+
+            const httpServer = require('http').createServer(app);
+            httpServer.listen({ port }, () => {
+                console.log('listening on port 3000!');
+
+                let args = [
+                    `http://0.0.0.0:${port}/`,
+                    outputFilePath
+                ];
+                if(options.pageSize) {
+                    args = args.concat([
+                        `--pageSize`,
+                        options.pageSize
+                    ]);
+                }
+                const electronPdf = child_process.spawn(
+                    path.join(__dirname, '../node_modules/electron-pdf/cli.js'),
+                    args
+                );
+
+                electronPdf.stdout.on('data', (data) => {
+                    console.log(`stdout: ${data}`);
+                });
+
+                electronPdf.stderr.on('data', (data) => {
+                    console.log(`stderr: ${data}`);
+                });
+
+                electronPdf.on('close', (code) => {
+                    console.log(`child process exited with code ${code}`);
+                    httpServer.close();
+                    resolve(outputFilePath);
+                });
+
+            });
+        }
+        else {
+            resolve();
+        }
+
+    });
+};
