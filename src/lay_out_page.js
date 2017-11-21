0e13b0f22d412407778248371d45496810d81290
David Stolarsky <david.stolarsky@gmail.com>
2017-11-21 08:53:32 -0500

    cleanup and margin!

--- src/lay_out_page.js
+++ src/lay_out_page.js
@@ -1,124 +1,20 @@
-var rbp = require('rectangle-bin-pack');
 var sum = require('lodash.sum');
 var range = require('lodash.range');
 
-const packWithScale = (images, pageSize, scale) => {
-    // console.error(`trying scale ${scale}`);
-    var rects = images.map(
-        image => ({
-            w: Math.floor(scale * image.metadata.width),
-            h: Math.floor(scale * image.metadata.height)
-        })
-    );
-    rbp.solveSync(
-        {
-            w: pageSize[0],
-            h: pageSize[1],
-            alg: 'BestAreaFit'
-            // BestShortSideFit or BSSF
-            // BestLongSideFit or BLSF
-            // BestAreaFit or BAF
-            // BottomLeftRule or BL
-            // ContactPointRule or CP
-        },
-        rects
-    );
-    const imagesSomePlaced = rects
-    .map((rect, r) => ({
-        image: images[r],
-        placement: rect
-    }));
 
-    const placedImages = imagesSomePlaced
-    .filter(
-        placedImage => placedImage.placement.hasOwnProperty('x') && placedImage.placement.hasOwnProperty('y')
-    );
-
-    const unplacedImages = imagesSomePlaced
-    .filter(
-        placedImage => !placedImage.placement.hasOwnProperty('x') || !placedImage.placement.hasOwnProperty('y')
-    );
-
-    const portionAreaUsed = sum(placedImages.map(
-        placedImage => placedImage.placement.w * placedImage.placement.h
-    )) / (pageSize[0] * pageSize[1]);
-
-    return {portionAreaUsed, placedImages, unplacedImages};
-};
-
-const rectangleBinPackStrategy = (images, pageSize, targetPhotosPerPage) => {
-    
-    console.error("Start rectangleBinPackStrategy. ", targetPhotosPerPage);
-
-    const targetPhotosPixelArea = sum(images.slice(0, targetPhotosPerPage).map(
-        image => image.metadata.width * image.metadata.height
-    ));
-    const pageMmArea = pageSize[0] * pageSize[1];
-
-
-
-    const sampleCount = 100;
-    let mostEfficientLayout;
-    let lowTargetScale = Math.sqrt(pageMmArea / targetPhotosPixelArea);
-    let highTargetScale = Math.sqrt(pageMmArea / targetPhotosPixelArea);
-
-    do {
-        lowTargetScale *= 0.9;
-        highTargetScale *= 1.1;
-        
-        const step = (highTargetScale - lowTargetScale) / sampleCount;
-        samples = range(lowTargetScale, highTargetScale, step)
-        .map(scale => packWithScale(
-            images,
-            pageSize,
-            scale
-        ))
-        .sort((a, b) => {
-            if(a.portionAreaUsed < b.portionAreaUsed) {
-                return -1;
-            }
-            else if(a.portionAreaUsed > b.portionAreaUsed) {
-                return 1;
-            }
-            else {
-                return 0;
-            }
-        });
-        mostEfficientLayout = samples[samples.length-1];
-    }
-    while(mostEfficientLayout.placedImages.length < 1);
-    // console.error(samples.map(sample => sample.portionAreaUsed));
-    // const max = Math.max.apply(Math, samples.map(sample => sample.portionAreaUsed));
-    // console.error('BEST: ', max);
-
-    // console.error(samples.map(sample => sample.portionAreaUsed));
-
-    
-
-    if(mostEfficientLayout.placedImages.length !== images.length) {
-        // console.error('################### Could not pack all images on the page!');
-    }
-
-    mostEfficientLayout.placedImages.forEach(placedImage => {
-        if(placedImage.placement.x + placedImage.placement.w > pageSize[0] ||
-            placedImage.placement.y + placedImage.placement.h > pageSize[1]) {
-            console.error('wtf ', placedImage.placement);
-        }
-    });
-
-    // else {
-        return {
-            placed: mostEfficientLayout.placedImages,
-            unplaced: mostEfficientLayout.unplacedImages
-        };
-    // }
-};
-    
-const rowPackStrategy = (images, pageSize, maxTargetPhotosPerRow) => {
+const rowPackStrategy = (images, pageSize, maxTargetPhotosPerRow, margin) => {
 
     let placedImages = [];
     let nextIndex = 0;
     var bottom = 0;
+
+    // each image adds 1 margin unit vertically and horizontally.
+    // the page will add the extra missing vertical and horizontal
+    // margin.
+    const imageSizeWithMargins = image => ({
+        width: image.metadata.width + margin,
+        height: image.metadata.height + margin
+    });
 
     while(
         placedImages.length < images.length
@@ -129,7 +25,7 @@ const rowPackStrategy = (images, pageSize, maxTargetPhotosPerRow) => {
 
         const nextRowImages = images.slice(nextIndex, nextIndex + targetNumberImagesNextRow);
         const rowAspectRatio = sum(nextRowImages.map(
-            nextRowImage => nextRowImage.metadata.width / nextRowImage.metadata.height
+            nextRowImage => imageSizeWithMargins(nextRowImage).width / imageSizeWithMargins(nextRowImage).height
         ));
         let rowHeight = pageSize[0] / rowAspectRatio;
         if(bottom === 0 && rowHeight > pageSize[1]) {
@@ -145,15 +41,15 @@ const rowPackStrategy = (images, pageSize, maxTargetPhotosPerRow) => {
         let x = 0;
         nextRowImages.forEach(
             nextRowImage => {
-                const width = rowHeight * nextRowImage.metadata.width / nextRowImage.metadata.height;
+                const width = rowHeight * imageSizeWithMargins(nextRowImage).width / imageSizeWithMargins(nextRowImage).height;
                 const imageBottom = bottom + rowHeight;
                 placedImages.push({
                     image: nextRowImage,
                     placement: {
                         x: x,
                         y: bottom,
-                        w: width,
-                        h: rowHeight
+                        w: width - margin,
+                        h: rowHeight - margin
                     },
                     bottom: () => imageBottom
                 });
@@ -162,10 +58,6 @@ const rowPackStrategy = (images, pageSize, maxTargetPhotosPerRow) => {
         );
 
         bottom += rowHeight;
-        // if(bottom > pageSize[1]) {
-        //     // console.error(`bottom is ${bottom}, page height is ${pageSize[1]}`);
-        //     break;
-        // }
     }
 
     return {
@@ -183,7 +75,7 @@ const verticallyCenter = (pageLayout, pageSize) => {
 };
 
 module.exports = (images, pageSize, maxTargetPhotosPerRow = 3) => {
-    const pageLayout = rowPackStrategy(images, pageSize, maxTargetPhotosPerRow);
+    const pageLayout = rowPackStrategy(images, pageSize, maxTargetPhotosPerRow, pageSize[0] * 0.02);
     if(/*pageLayout.placed.length === images.length &&*/ pageLayout.bottom < 0.5 * pageSize[1] && maxTargetPhotosPerRow > 1) {
         return module.exports(images, pageSize, maxTargetPhotosPerRow - 1)
     }
@@ -191,5 +83,4 @@ module.exports = (images, pageSize, maxTargetPhotosPerRow = 3) => {
         verticallyCenter(pageLayout, pageSize);
         return pageLayout;
     }
-    // return rectangleBinPackStrategy(images, pageSize, targetPhotosPerPage);
 };
